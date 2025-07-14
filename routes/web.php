cca75add6c031f34edaf3dde5c79cfd1092c8f7b
unknown <cyrhildwight@gmail.com>
2025-07-14 16:46:39 +0800

    login and register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'getLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'postLogin']);
+  
+    Route::get('/register', [AuthController::class, 'show'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
 });
+
