ba713301cd155463911850339b33ee403a4861d6
unknown <chaitherjurolan18@gmail.com>
2025-07-14 16:56:29 +0800

    hello

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::post('/timein', [HomeController::class, 'timeIn'])->name('timein');
+    Route::post('/timeout', [HomeController::class
+    , 'timeOut'])->name('timeout');
 });
